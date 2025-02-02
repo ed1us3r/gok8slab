64a67d1df238feef391ebcdd403f650bbd0a73c7
ed1us3r <Ed1us3r@outlook.de>
2025-02-02 13:16:23 +0100

    [INIT] colors.go  implemented

---
+++ internal/utils/colors.go
@@ -0,0 +1,31 @@
+package utils
+
+import "fmt"
+
+// ANSI Color Codes
+const (
+	Red    = "\033[31m"
+	Green  = "\033[32m"
+	Yellow = "\033[33m"
+	Blue   = "\033[34m"
+	Cyan   = "\033[36m"
+	Reset  = "\033[0m"
+)
+
+// Colorized output helpers
+func Info(msg string) {
+	fmt.Println(Blue + "[INFO] " + msg + Reset)
+}
+
+func Success(msg string) {
+	fmt.Println(Green + "[SUCCESS] " + msg + Reset)
+}
+
+func Warning(msg string) {
+	fmt.Println(Yellow + "[WARNING] " + msg + Reset)
+}
+
+func Error(msg string) {
+	fmt.Println(Red + "[ERROR] " + msg + Reset)
+}
+
